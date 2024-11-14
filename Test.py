c25e27956d461f54ef9c58e110f98ba51d588f74
kytman <kgkutman4@gmail.com>
2024-11-14 15:19:38 +0500

    тестовое изменение

---
+++ Test.py
@@ -0,0 +1,258 @@
+# 1
+# print("Hello")
+# print("Kutman")
+
+# 2
+# import this
+
+# 3
+# test = 'abc'
+"""
+test1 = 'abc'
+test2 = '123'
+test3 = 123
+"""
+
+# 4
+# a = 'abc'
+# print (a)
+# number = "12"
+# print (number)
+
+# 5
+# a = 'abc'
+# print (a)
+# number = "12"
+# print (number)
+
+# 6
+# test = 1 + 2 + 3
+# print (test)
+# test1 = 4
+# test2 = 6
+# print (test2 - test1)
+# print (test1 * test2)
+# test3 = 8
+# test4 = 3
+# print (test3 / test4)
+# print (test3 // test4)
+
+# 7
+# num1 = 15
+# num2 = 3
+# print (num1 + num2)
+# print (num1 - num2)
+# print (num1 * num2)
+# print (num1 / num2)
+# print (num1 // num2)
+
+# 8
+# num = 4
+# # num += 1
+# # num -= 1
+# # num += 1
+# # print (num)
+
+# 9
+# res = 2 * 2 + 3 # выведет 7 (результат 4 + 3)
+# print(res)
+
+# 10
+# res = 8 / 2 * 4
+# print(res)
+# res = 8 * 2 / 4
+# print(res)
+# res = 16 / 2 / 2 / 2
+# print(res)
+
+# 11
+# res = 2 * (2 + 3)
+# print(res)
+# res = 2 * (2 + 4 * (3 + 1))
+# print(res)
+
+# 12
+# res = (2 * 2) + 3
+# print(res)
+# res = 8 / 2 * 4
+# print(res)
+# res = (8 / 2) * 4
+# print(res)
+
+# 13
+# num1 = 200 ** 500
+# num2 = 3000
+# print (num1 + num2)
+# num1 = 67 ** 50
+# num2 = 45 ** 60
+# print (num1 - num2)
+
+# 14
+# res = 0.5 + 0.5
+# print(res)
+# num1 = 1.5
+# num2 = 0.75
+# print (num1 + num2)
+# num1 = 3.4
+# num2 = 8.27
+# num3 = 1.5
+# res = num2 - num1 * num3
+# print (res)
+
+# 15
+# test = -100
+# print (test)
+# print (-test)
+# print (test - -20)
+
+# 16
+# num1 = 13
+# num2 = 3
+# print (num1 % num2)
+# num1 = 26
+# num2 = 8
+# print (num1 % num2)
+# num1 = -5
+# num2 = 12
+# print (num2 % num1)
+
+# 17
+# num1 = 2
+# num2 = 5
+# print (num1 ** num2)
+# print (5 ** (num1 + num2))
+
+# 18
+# print(2 * 2 ** 3)
+
+# 19
+# txt = 'abc'
+# print(txt)
+
+# 20
+# test = 'abc' + 'def' # складываем две строки
+# print(test)
+# test1 = 'abc'
+# test2 = 'def'
+# test3 = test1 + test2 # складываем две строки
+# print(test3) # выведет 'abcdef'
+# txt1 = 'abc'
+# # txt2 = 'def'
+# # print (txt1 + txt2)
+# txt1 = '12'
+# txt2 = '+'
+# txt3 = '5'
+# txt4 = '17'
+# print (txt1 + txt2 + txt3 + txt4)
+# txt1 = '12'
+# txt2 = '+'
+# txt3 = '5'
+# txt4 = '='
+# txt5 = '17'
+# print (txt1 + txt2 + txt3 + txt4 + txt5)
+
+# 21
+# print(3 * 'abc')
+# print(4 * 'a1b2')
+# print(2 * 'ab' + '1213')
+
+# 22
+# txt = 'abcde'
+# print(txt[1]) # выведет 'b'
+# txt = 'abcde'
+# print(txt[-1]) # выведет 'e'
+# txt = 'abcdef'
+# print(txt[0])
+# print(txt[1])
+# print(txt[-1])
+# print(txt[-2])
+
+# 23
+# tst = 'sfgs\'sdf'
+# print(tst)
+# tst = "abcd\"fr'23"
+# print(tst)
+
+# 24
+# Количество символов строки (ее длину) можно найти с помощью функции len:
+# Пробел также является символом:
+# txt = 'ab c'
+# print(len(txt)) # выведет 4
+# txt = 'abcdef'
+# print(len(txt))
+# txt = 'ab cd ef'
+# print(len(txt))
+
+# 25
+# print('''
+# 	1
+# 	2
+# 	3
+# ''')
+# print('''
+# 	Прим
+# 	Кут
+# 	Алт
+# ''')
+
+# 28
+# isAdult = False
+# isAdult = True
+# print(isAdult)
+
+#30
+# test = None
+# print(test)
+
+#31
+# num1 = 1
+# num2 = 2
+# num3 = num1 + num2
+# print(num3)
+#
+# tst1 = 'abc'
+# txt2 = 123
+# txt3 = tst1 + str(txt2)
+# print(txt3)
+
+# tst ='123'
+# print(len(tst))
+
+# tst = 123
+# print(tst)
+#
+# tst3 = tst % 10 # 3
+#
+# print(tst2 + txt2 + tst3)
+
+#32
+# txt = '123'
+# print(int(txt))
+# txt1 = '123'
+# txt2 = '456'
+# print(int(txt1)+int(txt2))
+
+#33
+# txt = '123'
+# print(txt[0] + txt[1]) # выведет '12' - суммирует как строки
+# txt = '123'
+# print(int(txt[0]) + int(txt[1])) # выведет 3
+# txt = '12345'
+# print(int(txt[0]) + int(txt[1]) + int(txt[2]) + int(txt[3]) + int(txt[4]))
+# txt = '2489'
+# print((int(txt[2])-int(txt[1]))*int(txt[0]))
+
+#34
+# txt = '123'
+# print(int(txt[0]) + int(txt[1]) + int(txt[2]))
+
+#35
+# test1 = '1.1'
+# test2 = '2.1'
+# test3 = float(test1) + float(test2)
+# print(test3)
+
+#36
+# tst1 = 2
+# tst2 = 4
+# print(float(tst2) - float(tst1))
